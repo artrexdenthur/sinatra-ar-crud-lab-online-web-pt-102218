bb4f4777a631af1bfd77d26419966bb1ecccaaa2
Paul Ashour <paul.ashour@gmail.com>
2019-02-28 04:54:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,7 +45,7 @@ class ApplicationController < Sinatra::Base
   end
   
   delete '/articles/:id' do
-    binding.pry
+    # binding.pry
     Article.find(params[:id]).delete
     redirect '/articles'
   end
