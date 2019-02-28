a4fe194d5c4a48278aceb19eecd7018eaebc42fe
Paul Ashour <paul.ashour@gmail.com>
2019-02-28 04:44:12 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,6 +41,7 @@ class ApplicationController < Sinatra::Base
     @article.title = params[:title]
     @article.content = params[:content]
     @article.save
+    redirect "/articles/#{params[:id]}"
   end
   
 end
