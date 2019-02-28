10acac57cd8eb448555b75c98521976b310a5aa5
Paul Ashour <paul.ashour@gmail.com>
2019-02-28 04:42:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,6 +38,8 @@ class ApplicationController < Sinatra::Base
   
   patch '/articles/:id' do
     @article = Article.find(params[:id])
+    @article.title = params[:title]
+    @article.content = params[:content]
   end
   
 end
